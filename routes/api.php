51549b6c19dc1d4aebf035f3e6d5a4c0342a184e
ryanf27 <ryanfebry0227@gmail.com>
2024-02-11 22:43:34 +0700

    add routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\JobController;
+use App\Http\Controllers\EmployerController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +17,19 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
+// Jobs
+Route::apiResource('/jobs', JobController::class);
+
+// Employers
+Route::apiResource('/employers', EmployerController::class);
+
+// Users
+Route::apiResource('/users', UserController::class);
+
+// Categories
+Route::apiResource('/categories', CategoryController::class);
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
